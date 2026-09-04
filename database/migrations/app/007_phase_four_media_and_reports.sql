6302b2a4bb48b80cfe9a14c500b7dc2b31e3ac46
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-04 17:38:31 +0300

    Make Phase 4 report integrity migration safe

--- database/migrations/app/007_phase_four_media_and_reports.sql
+++ database/migrations/app/007_phase_four_media_and_reports.sql
@@ -1,3 +1,10 @@
+DELETE older
+FROM flash_reports older
+JOIN flash_reports newer
+    ON newer.flash_id = older.flash_id
+    AND newer.reporter_user_id = older.reporter_user_id
+    AND newer.id > older.id;
+
 ALTER TABLE flash_reports
     ADD UNIQUE KEY uq_flash_reporter (flash_id, reporter_user_id);
 
