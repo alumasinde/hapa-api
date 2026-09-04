d4c1b6e34e3ceddbda62735d95b8f0b55883031e
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-04 14:38:29 +0300

    Build Phase 1 backend foundation

---
+++ app/Support/Date.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Support;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+use DateTimeZone;
+
+final class Date
+{
+    public static function now(): DateTimeImmutable
+    {
+        return new DateTimeImmutable('now', new DateTimeZone('UTC'));
+    }
+
+    public static function iso(DateTimeInterface $date): string
+    {
+        return $date->setTimezone(new DateTimeZone('UTC'))->format(DateTimeInterface::ATOM);
+    }
+
+    public static function expiresInMinutes(int $minutes): DateTimeImmutable
+    {
+        return self::now()->modify(sprintf('+%d minutes', $minutes));
+    }
+}
